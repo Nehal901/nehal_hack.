1dfd0f9350821c7d75faf09fa70f357d232fa229
Nehal901 <129887011+Nehal901@users.noreply.github.com>
2023-04-26 13:32:27 +0600

    Add files via upload

---
+++ nehal_hack.py
@@ -0,0 +1,43 @@
+import os
+os.system("clear")
+
+logo = ("""
+\033[31;1m                   
+███    ██ ███████ ██   ██  █████  ██      
+████   ██ ██      ██   ██ ██   ██ ██      
+██ ██  ██ █████   ███████ ███████ ██      
+██  ██ ██ ██      ██   ██ ██   ██ ██      
+██   ████ ███████ ██   ██ ██   ██ ███████
+                                                    
+                                                    
+
+\033[32;1m                  
+\033[33;1m                 
+\033[34;1m                
+\033[35;1m               
+\033[32;1m           ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+\033[32;1m           ┃\033[34;1m TOOLS NAME => FB-HACKING               ┃
+\033[32;1m           ┃\033[32;1m OWNER NAME => Nehal khan                 ┃
+\033[32;1m           ┃\033[33;1m CLONE SYS  => DATA , WIFI                     ┃
+\033[32;1m           ┃\033[32;1m WHATS APP  => +880177****                 ┃
+\033[32;1m           ┃\033[35;1m FACEBOOK   => Nehal Khan                     ┃
+\033[32;1m           ┃\033[31;1m      PLEASE DON'T UES A RONG WORK   ┃
+\033[32;1m           ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛""")
+
+print(logo)
+print("[1] FACEBOOK HACK")
+print("[2] GMAIL HACK")
+print("[3] WHATSAPP HACK")
+print("[4] IMO HACK")
+print("[5] TIKTOK HACK")
+print("[6] FREEFIRE HACK")
+
+x = input ('CHOICE : ') 
+
+os.system("clear")
+print(logo)
+x = input ('ENTER TERGET ID : ') 
+
+x = input ('HACKING START PLZ ENTER ')
+
+x = input ('HACKED SUCCESSFULL ✅ YOUR TARGET PASSWORD : BLACK143##')
